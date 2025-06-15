82c8f318c33c5817c524bdeaf5821b131872018d
leo <leoyfm@gmail.com>
2025-06-15 17:47:03 +0800

    添加 用户交易功能

--- backend/app/api/v1/user/__init__.py
+++ backend/app/api/v1/user/__init__.py
@@ -4,6 +4,7 @@
 from fastapi import APIRouter
 from .profile import router as profile_router
 from .cards import router as cards_router
+from .transactions import router as transactions_router
 from .reminders import router as reminders_router
 from .recommendations import router as recommendations_router
 
@@ -13,6 +14,7 @@ user_router = APIRouter(prefix="/user", tags=["v1-用户功能"])
 # 注册子路由
 user_router.include_router(profile_router)
 user_router.include_router(cards_router)
+user_router.include_router(transactions_router)
 user_router.include_router(reminders_router)
 user_router.include_router(recommendations_router)
 
